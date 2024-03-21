987a5da8d886691d9f82477c6ce5565c2c8c38ca
Max Makinen <quantumhound@gmail.com>
2024-03-21 17:02:29 +0200

    added comment

--- Inventory/InventoryUI/UIItemSlot/UI_item_slot.gd
+++ Inventory/InventoryUI/UIItemSlot/UI_item_slot.gd
@@ -3,12 +3,9 @@ extends TextureRect
 @onready var item_icon: TextureRect = %ItemIcon
 @onready var item_amount: Label = %ItemAmount
 
-
-
+# Display item sprite and amount that are found inside the InventorySlot
 func display_item(item: InventorySlot) -> void:
 	if item != null and item.item != null:
-		# TODO : In case we decide for proper individual sprites
-		#item_icon.texture = load("res://Assets/crafting_materials/%s" % item.item.name)
 		item_icon.texture = item.item.texture
 		item_amount.text = str(item.amount) if item.is_stackable() else ""
 	else:
