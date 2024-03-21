58e971d6d7200b69688ee9ef8b4f478f9a4e6386
Max Makinen <quantumhound@gmail.com>
2024-03-21 13:47:28 +0200

    testing new inventory ui slots

---
+++ Inventory/InventoryUI/UIItemSlot/UI_item_slot.gd
@@ -0,0 +1,14 @@
+extends ColorRect
+
+@onready var item_icon: TextureRect = $ItemIcon
+@onready var item_amount: Label = $ItemAmount
+
+func display_item(item: InventorySlot) -> void:
+	if item != null:
+		# TODO : In case we decide for proper individual sprites
+		#item_icon.texture = load("res://Assets/crafting_materials/%s" % item.item.name)
+		item_icon.texture = item.item.texture
+		item_amount.text = str(item.amount) if item.is_stackable() else ""
+	else:
+		item_icon.texture = null
+		item_amount.text = ""
