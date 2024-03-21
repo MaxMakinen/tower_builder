b6d5d5001602d840c9caebfa7b667852b8a41637
Max Makinen <quantumhound@gmail.com>
2024-03-21 17:19:36 +0200

    Added todo

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -31,7 +31,7 @@ func _physics_process(_delta):
 	velocity.y = move_toward(velocity.y, speed * direction.y, acceleration)
 	move_and_slide()
 
-
+# TODO : It's broken, fex soon
 func add_to_inventory(item: ItemResource):
 	inventory.insert(item)
 
