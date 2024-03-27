09bf4946ed2a334e298c3fe1f9ba517242ecb41d
Max Makinen <quantumhound@gmail.com>
2024-03-27 10:09:08 +0200

    added comments

--- Item/Containers/world_item_container.gd
+++ Item/Containers/world_item_container.gd
@@ -17,6 +17,7 @@ signal open_container(inventory)
 
 # PRIVATE FUNCTIONS
 
+# When constructing new container, adjust container size according to input or use default value of 1
 func _init(size: int = container_size) -> void:
 	container_size = size
 	if inventory == null:
@@ -24,7 +25,7 @@ func _init(size: int = container_size) -> void:
 	else:
 		inventory.set_inventory_size(container_size)
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time. connect to interaction area signal
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_interact")
 #	if inventory == null:
@@ -42,22 +43,25 @@ func _on_interact():
 
 
 # PUBLIC FUNCTIONS
+
+# Change flag to mark container as free to be interacted with again
 func close_container() -> void:
 	_interactable = true
 
-
+# Change container inventory to new inventory and change container size to match
 func set_inventory(new_inventory: Inventory) -> void:
 	inventory = new_inventory
+	container_size = inventory.get_inventory_size()
 
-
+# Return container inventory object
 func get_inventory() -> Inventory:
 	return inventory
 
-
+# Change container size and adjust inventory to match
 func set_container_size(size: int) -> void:
 	container_size = size
 	inventory.set_inventory_size(container_size)
 
-
+# Return size of container as int
 func get_container_size() -> int:
 	return container_size
