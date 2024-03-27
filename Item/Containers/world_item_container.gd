4b021c7e6ac31748093535a23ba3f6faf530cd30
Max Makinen <quantumhound@gmail.com>
2024-03-27 09:20:05 +0200

    Should react to interaction area

--- Item/Containers/world_item_container.gd
+++ Item/Containers/world_item_container.gd
@@ -2,9 +2,11 @@ extends StaticBody2D
 
 class_name WorldItemContainer
 
+@onready var interaction_area: InteractionArea = $InteractionArea
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	interaction_area.interact = Callable(self, "_on_interact")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
