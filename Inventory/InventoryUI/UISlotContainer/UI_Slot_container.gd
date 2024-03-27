62aa65ded48abc8649daf14778de412616a637df
Max Makinen <quantumhound@gmail.com>
2024-03-27 10:24:04 +0200

    commented out test print statement

--- Inventory/InventoryUI/UISlotContainer/UI_Slot_container.gd
+++ Inventory/InventoryUI/UISlotContainer/UI_Slot_container.gd
@@ -46,8 +46,8 @@ func _on_inventory_update(indices: Array[int]) -> void:
 		if get_child_count() == inventory.get_inventory_size() and index >= 0 and index < inventory.get_inventory_size():
 			var item_slot = get_child(index)
 			if item_slot:
-				print("Inv Update at index : ", index)
 				item_slot.display_item(inventory.get_item_at(index))
+				#print("Inv Update at index : ", index)
 
 
 # Set new value for selected
