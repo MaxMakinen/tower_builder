c7105b460e46ff223e490959a35346e1d4da38b7
Max Makinen <quantumhound@gmail.com>
2024-03-28 13:52:28 +0200

    Removing extras from dra_preview and starting on manager script

--- UI/DragPreview/Drag_preview.gd
+++ UI/DragPreview/Drag_preview.gd
@@ -6,16 +6,6 @@ class_name DragPreview
 @onready var item_amount: Label = %ItemAmount
 
 var _dragged_item: InventorySlot = null
-enum DragID {
-	EMPTY,
-	INV_ITEM,
-	HOTBAR_ITEM
-	}
-var _dragged_ID: DragID = DragID.EMPTY
-
-var previous_inventory: Inventory = null
-var previous_index: int = 0
-
 
 # If we are dragging an item, _dragged_item is not null, make node follow mouse position
 func _process(_delta: float) -> void:
@@ -23,41 +13,26 @@ func _process(_delta: float) -> void:
 		position = get_global_mouse_position()
 
 
-func set_dragged_item(item: InventorySlot, id: int) -> void:
+func set_dragged_item(item: InventorySlot) -> void:
 	_dragged_item = item
-	_dragged_ID = id as DragID
 	if _dragged_item != null and _dragged_item.item != null:
 		item_icon.texture = item.item.texture
 		item_amount.text = str(item.get_amount()) if item.is_stackable() else ""
 	else:
 		item_icon.texture = null
 		item_amount.text = ""
-		previous_inventory = null
-		previous_index = 0
 
-func set_backup(inventory: Inventory, index: int) -> void:
-	previous_inventory = inventory
-	previous_index = index
-
-func return_backup() -> InventorySlot:
-	var temp = _dragged_item
-	if !previous_inventory.attempt_insert_at(_dragged_item.get_item(), previous_index, _dragged_item.get_amount()):
-		print("Returning backup from dragged item failed")
-		return null
-	set_dragged_item(null, 0)
-	return temp
-
-func get_dragID() -> int:
-	return _dragged_ID
 
 func change_amount(amount: int) -> void:
 	_dragged_item.change_amount(amount)
 	item_amount.text = str(_dragged_item.get_amount())
 	if _dragged_item.get_amount() < 1:
-		set_dragged_item(null, DragID.EMPTY)
+		set_dragged_item(null)
+
 
 func get_amount() -> int:
 	return _dragged_item.get_amount()
+
 
 func get_dragged_item() -> InventorySlot:
 	return _dragged_item
