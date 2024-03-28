5e3c628c03cca0718c0503b2eedbda6ca685b618
Max Makinen <quantumhound@gmail.com>
2024-03-28 09:54:56 +0200

    new variable with setter determines selected hotbar slot

--- Inventory/InventoryUI/UIHotbar/UI_hotbar.gd
+++ Inventory/InventoryUI/UIHotbar/UI_hotbar.gd
@@ -5,24 +5,25 @@ extends SlotContainer
 #const UI_HOTBAR_SLOT = preload("res://Inventory/InventoryUI/UIHotbar/UI_hotbar_slot.tscn")
 var hotbar_size = 6
 
+# TODO : Testing setter and getters, might be useful, might not
 # Keep track of selected hotbar slot, send signal in case of change
 signal hotbar_changed(selection)
-var _hotbar_selection: int:
-	set(selection):
-		hotbar_changed.emit(selection)
-		_hotbar_selection = selection
-	get:
-		return _hotbar_selection
+var _hotbar_selection: int = 0: set = _set_hotbar_selection
+#	set(selection):
+#		hotbar_changed.emit(selection)
+#		_hotbar_selection = selection
+#	get:
+#		return _hotbar_selection
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	columns = hotbar_size
-	_selected = 0
 	if !inventory or !inventory.item_slots:
 		inventory = Inventory.new(hotbar_size)
 	inventory.set_selected(_selected) 
 	connect_to_inventory()
 	display_items()
+	_hotbar_selection = 0
 
 
 func set_hotbar_size(new_size: int) -> void:
@@ -39,22 +40,28 @@ func load_hotbar(new_inventory: Inventory) -> void:
 	columns = inventory.get_inventory_size()
 	display_items()
 
-# TODO : Needs to reference slots, not inventory
+
+func _set_hotbar_selection(new_selection: int) -> void:
+	get_child(_hotbar_selection).unselect()
+	get_child(new_selection).select()
+	_hotbar_selection = new_selection
+	hotbar_changed.emit(_hotbar_selection)
+
 # Highlight selected slot
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.is_action_pressed("1"):
-			inventory.set_selected(0)
+			_hotbar_selection = 0
 		if event.is_action_pressed("2"):
-			inventory.set_selected(1)
+			_hotbar_selection = 1
 		if event.is_action_pressed("3"):
-			inventory.set_selected(2)
+			_hotbar_selection = 2
 		if event.is_action_pressed("4"):
-			inventory.set_selected(3)
+			_hotbar_selection = 3
 		if event.is_action_pressed("5"):
-			inventory.set_selected(4)
+			_hotbar_selection = 4
 		if event.is_action_pressed("6"):
-			inventory.set_selected(5)
+			_hotbar_selection = 5
 #		if event.is_action_pressed("7"):
 #			inventory.set_selected(6)
 #		if event.is_action_pressed("8"):
