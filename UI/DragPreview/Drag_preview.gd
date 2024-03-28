e1abcd70c7cb81d80488456cdbbe241ef5c9208e
Max Makinen <quantumhound@gmail.com>
2024-03-28 11:06:22 +0200

    Tryind an enum identifier to determine what is being dragged

--- UI/DragPreview/Drag_preview.gd
+++ UI/DragPreview/Drag_preview.gd
@@ -6,7 +6,12 @@ class_name DragPreview
 @onready var item_amount: Label = %ItemAmount
 
 var _dragged_item: InventorySlot = null
-
+enum drag_ID {
+	EMPTY,
+	INV_ITEM,
+	HOTBAR_ITEM
+	}
+var _dragged_ID: drag_ID
 
 # If we are dragging an item, _dragged_item is not null, make node follow mouse position
 func _process(_delta: float) -> void:
@@ -14,8 +19,9 @@ func _process(_delta: float) -> void:
 		position = get_global_mouse_position()
 
 
-func set_dragged_item(item: InventorySlot) -> void:
+func set_dragged_item(item: InventorySlot, id: drag_ID) -> void:
 	_dragged_item = item
+	_dragged_ID = id
 	if _dragged_item != null and _dragged_item.item != null:
 		item_icon.texture = item.item.texture
 		item_amount.text = str(item.get_amount()) if item.is_stackable() else ""
@@ -23,12 +29,14 @@ func set_dragged_item(item: InventorySlot) -> void:
 		item_icon.texture = null
 		item_amount.text = ""
 
+func get_drag_id() -> int:
+	return _dragged_ID
 
 func change_amount(amount: int) -> void:
 	_dragged_item.change_amount(amount)
 	item_amount.text = str(_dragged_item.get_amount())
 	if _dragged_item.get_amount() < 1:
-		set_dragged_item(null)
+		set_dragged_item(null, 0)
 
 func get_amount() -> int:
 	return _dragged_item.get_amount()
