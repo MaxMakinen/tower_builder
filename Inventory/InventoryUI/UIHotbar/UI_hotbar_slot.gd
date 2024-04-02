1adbf78d48ca4d9881128639aeb46220565a4de7
Max Makinen <quantumhound@gmail.com>
2024-04-02 10:20:34 +0300

    Testing new signal

--- Inventory/InventoryUI/UIHotbar/UI_hotbar_slot.gd
+++ Inventory/InventoryUI/UIHotbar/UI_hotbar_slot.gd
@@ -7,23 +7,41 @@ extends UIItemSlot
 var _total_amount: int
 var content: InventorySlot
 
-# Hotbar signals
+# SIGNALS
 signal hotbar_slot_changed()
 
 func _ready() -> void:
 	_slot_type = SlotManager.SlotType.HOTBAR
 
-# Display item sprite and amount that are found inside the InventorySlot If nothing found in slot then display empty
-func display_item(item: InventorySlot) -> void:
-	if item != null and item.get_item() != null:
-		content = item
-		item_icon.texture = item.get_texture()
-		item_amount.text = str(_total_amount) if item.is_stackable() else ""
-		# TODO : Should we replace custom tooltip with built-in option and customize that?
-		#set_tooltip_text(item.item.name)
+## Display item sprite and amount that are found inside the InventorySlot If nothing found in slot then display empty
+#func display_item(item: InventorySlot) -> void:
+#	if item != null and item.get_item() != null:
+#		content = item
+#		item_icon.texture = item.get_texture()
+#		item_amount.text = str(_total_amount) if item.is_stackable() else ""
+#		# TODO : Should we replace custom tooltip with built-in option and customize that?
+#		#set_tooltip_text(item.item.name)
+#	else:
+#		item_icon.texture = null
+#		item_amount.text = ""
+
+
+func display_contents() -> void:
+	if _contents != null and !_contents.is_empty():
+		item_icon.texture = _contents.get_texture()
+		item_amount.text = str(_total_amount) if _contents.is_stackable() else ""
 	else:
 		item_icon.texture = null
 		item_amount.text = ""
+
+
+func set_hotbar_slot(item: InventorySlot) -> void:
+	_total_amount = get_parent().get_total_amount(item)
+	_contents = item
+	_contents.slot_changed.connect(display_contents)
+	_contents.slot_empty.connect(display_contents)
+	hotbar_slot_changed.emit()
+	display_contents()
 
 # Sets total amount to new value
 func set_total_amount(new_amount: int) -> void:
