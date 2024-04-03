54b3d505da450cb7631e67703ab2f4618b365717
Max Makinen <quantumhound@gmail.com>
2024-04-03 17:13:33 +0300

    smelter setup

--- Crafting/smelter.gd
+++ Crafting/smelter.gd
@@ -1,5 +1,6 @@
 extends CraftingNode
 
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+# Called when player interacts with interaction area
+func _on_interact():
+	print("INTERACTION")
+	print("Start Crafting")
 	pass
