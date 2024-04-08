b8bcddb3a006dbe2dfdfefbe3bfd83aea784d32f
Max Makinen <quantumhound@gmail.com>
2024-04-08 13:34:16 +0300

    Crafting pillar glow tween always starts from transparent

--- Crafting/Crafting_node.gd
+++ Crafting/Crafting_node.gd
@@ -10,7 +10,7 @@ var animation_tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_animate(false)
+	glow.hide()
 	hit_box.targeted.connect(_animate)
 	hit_box.attempt_interaction.connect(_on_interact)
 
@@ -21,15 +21,17 @@ func _process(_delta: float) -> void:
 # If targeted is true then start animation tween. Otherwise kill tween and hide glow sprite
 func _animate(targeted: bool) -> void:
 	if targeted == true:
+		glow.modulate = Color.TRANSPARENT
 		glow.show()
 		animation_tween = create_tween().set_loops()
-		animation_tween.tween_property(glow, "modulate", Color.TRANSPARENT, 1)#.set_ease(Tween.EASE_IN_OUT)
+		animation_tween.tween_property(glow, "modulate", Color(1, 1, 1), 1)#.set_ease(Tween.EASE_IN_OUT)
 		animation_tween.tween_interval(0.1)
-		animation_tween.tween_property(glow, "modulate", Color(1, 1, 1), 1).set_ease(Tween.EASE_IN_OUT)
+		animation_tween.tween_property(glow, "modulate", Color.TRANSPARENT, 1)#.set_ease(Tween.EASE_IN_OUT)
 		animation_tween.tween_interval(0.2)
 	else:
 		glow.hide()
-		animation_tween.kill()
+		if animation_tween:
+			animation_tween.kill()
 
 
 # Called when player interacts with interaction area
