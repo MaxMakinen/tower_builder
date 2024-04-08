64bbc0a93e90b01a80edf2b09093aaba7ae5e78b
Max Makinen <quantumhound@gmail.com>
2024-04-08 11:35:12 +0300

    added signal for detecting harvest attempt

--- Interaction/HitBox/HitBox.gd
+++ Interaction/HitBox/HitBox.gd
@@ -3,6 +3,8 @@ extends Area2D
 enum OWNER_TYPE {RESOURCE, INTERACTABLE, ENEMY, NPC}
 @export var owner_type : OWNER_TYPE
 
+signal attempt_harvest
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,5 +20,6 @@ func _on_area_entered(area: Area2D) -> void:
 	print("area name : ", area.name)
 	if owner_type == OWNER_TYPE.RESOURCE and area.name == "StrikeArea":
 		print("Resource Gathering Attack")
+		attempt_harvest.emit()
 		pass
 	pass # Replace with function body.
