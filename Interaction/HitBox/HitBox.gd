af7899fd92c3cd215454bf18acf3b86490392190
Max Makinen <quantumhound@gmail.com>
2024-04-08 11:34:00 +0300

    Removed signal for body entered and added comment

--- Interaction/HitBox/HitBox.gd
+++ Interaction/HitBox/HitBox.gd
@@ -12,18 +12,11 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-
-func _on_body_entered(body: Node2D) -> void:
-	print("Entry detected")
-	if owner_type == OWNER_TYPE.RESOURCE and body.name == "player":
-		print("Resource Gathering Attack")
-		pass
-	pass # Replace with function body.
-
-
+# Detect incoming interaction attempt and respond accordingly
 func _on_area_entered(area: Area2D) -> void:
 	print("Area Entry detected")
-	if owner_type == OWNER_TYPE.RESOURCE and area.name == "player":
+	print("area name : ", area.name)
+	if owner_type == OWNER_TYPE.RESOURCE and area.name == "StrikeArea":
 		print("Resource Gathering Attack")
 		pass
 	pass # Replace with function body.
