b6c5f606c4b1436724bd7ae5f726c91478d8ccc3
Max Makinen <quantumhound@gmail.com>
2024-04-09 12:29:19 +0300

    Expanding on item variables

--- Item/Item_resource.gd
+++ Item/Item_resource.gd
@@ -4,7 +4,9 @@ extends Resource
 class_name ItemResource
 
 @export var name: String = "Default name"
-@export_enum("Stone", "Wood", "Ore", "Metal", "Ingot", "fuel", "misc") var type: String = "Wood"
+#@export_enum("Stone", "Wood", "Ore", "Metal", "Ingot", "fuel", "misc") var type: String = "Wood"
+@export_enum("LOG", "ORE", "INGOT", "CHUNK", "WEAPON", "TOOL", "MISC") var type: String = "Log"
+@export var material: MaterialResource
 @export var texture: Texture2D
 @export var max_stack_size: int = 99
 @export var description: String = "Default description"
