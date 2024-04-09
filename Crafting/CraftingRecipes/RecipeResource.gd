19e9219e22dcbec8f27da770bad136501a31ed3f
Max Makinen <quantumhound@gmail.com>
2024-04-09 13:11:26 +0300

    added more variables

--- Crafting/CraftingRecipes/RecipeResource.gd
+++ Crafting/CraftingRecipes/RecipeResource.gd
@@ -3,11 +3,12 @@ extends Resource
 
 class_name RecipeResource
 
-@export var name: String
+
 @export var output: ItemResource
+@export var name: String = "Unnamed Object"
+@export var description: String = "Make thing from stuff"
 
-
-var required_flags: Dictionary = {
+@export var required_flags: Dictionary = {
 	"TYPE" : ["ORE"],
 	"NATURE" : ["METAL"],
 	"SOLIDITY" : ["SOLID"],
